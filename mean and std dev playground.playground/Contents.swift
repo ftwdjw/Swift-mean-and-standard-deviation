f00a5e7618faadb4e4bafc8c68553988611aabfc
ftwdjw <ftwdjw@gmail.com>
2016-09-13 10:20:37 -0500

    initial commit xcode 8 tested

---
+++ mean and std dev playground.playground/Contents.swift
@@ -0,0 +1,85 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Accelerate
+
+var str = "Hello, playground"
+
+let testArray: [Int8] = [0, 1, 2, 3, 4]
+
+func IntToDouble (a: [Int8] ) -> [Double] {
+    
+    //Converts an array of integer values to Double values
+    
+    
+    var result = [Double](repeating:0, count: a.count)
+    
+    //vDSP_dotprD(a, 1, b, 1, &result, UInt(a.count))
+    //vDSP_vsmulD(a, 1, &bb, &result, 1, UInt(a.count))
+    //vDSP_vfixr8D(a, 1, &result, 1, UInt(a.count))
+    vDSP_vflt8D( a, 1, &result, 1, UInt(a.count))
+    return result
+}
+
+func meanStdDev (a: [Double] ) -> (mean: Double, standardDeviation: Double) {
+    
+    //Calculaltes the mean and the standar deviation of an array of Double numbers
+    
+    //To calculate the standard deviation of those numbers:
+    /*
+     Work out the Mean (the simple average of the numbers)
+     Then for each number: subtract the Mean and square the result.
+     Then work out the mean of those squared differences.
+     Take the square root of that and we are done!*/
+    
+    print("input array=\(a)")
+    
+    
+    var myMean = 0.0
+    var meanOfSquaredValues = 0.0
+    var myStandardDeviation = 0.0
+    var sum = 0.0
+    var sumOfSquares = 0.0
+    var difference = [Double](repeating:0.0, count: a.count)
+    var squareOfDifference = [Double](repeating:0.0, count: a.count)
+    
+    UInt(a.count)
+    
+    vDSP_sveD(a, 1, &sum, UInt(a.count))
+    sum
+    
+    myMean = sum/Double(a.count)
+    
+    print("mean=\(myMean)")
+    
+    var minusMean = -myMean
+    
+    
+    vDSP_vsaddD(a, 1, &minusMean, &difference, 1, UInt(a.count))
+    vDSP_vsqD(difference, 1, &squareOfDifference, 1, UInt(a.count))
+    vDSP_sveD(squareOfDifference, 1, &sumOfSquares, UInt(a.count))
+    
+    meanOfSquaredValues = sumOfSquares/Double(a.count)
+    
+    myStandardDeviation = sqrt(meanOfSquaredValues)
+    
+    print("standard deviation=\(myStandardDeviation)")
+    
+    return (mean: myMean, standardDeviation: myStandardDeviation)
+}
+
+
+let doubleTestArrray = IntToDouble(a: testArray )
+
+print("double test array=\(doubleTestArrray)")
+
+let mean = meanStdDev(a: doubleTestArrray).mean
+
+let standardDev = meanStdDev(a: doubleTestArrray).standardDeviation
+
+
+print("mean=\(mean)")
+
+    print("standard deviation=\(standardDev)")
+
+
